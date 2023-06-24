821290fbec1306594fb3167013f4bc22fd67eb7c
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-06-24 11:42:33 -0300

    feat: fist commit springboot kotlin project

---
+++ Kotlin_REST_API/src/main/kotlin/br/com/erudio/Kotlin_REST_API/KotlinRestApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.erudio.Kotlin_REST_API
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestApiApplication>(*args)
+}
