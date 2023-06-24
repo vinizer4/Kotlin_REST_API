695122b9e66f118edd70ebcc4571b3bdb4832464
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-06-24 17:41:46 -0300

    feat: created person endpoints: post, put, delete.

--- Kotlin_REST_API/src/main/kotlin/br/com/erudio/Kotlin_REST_API/controllers/PersonController.kt
+++ Kotlin_REST_API/src/main/kotlin/br/com/erudio/Kotlin_REST_API/controllers/PersonController.kt
@@ -5,6 +5,7 @@ import br.com.erudio.Kotlin_REST_API.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -16,6 +17,13 @@ class PersonController {
     @Autowired
     private lateinit var service: PersonService
 
+    @RequestMapping(method = [RequestMethod.GET],
+            produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun findAll(): List<Person> {
+        return service.findAll()
+    }
+
     @RequestMapping(
             value = ["/{id}"],
             method = [RequestMethod.GET],
@@ -25,11 +33,30 @@ class PersonController {
         return service.findById(id)
     }
 
-    @RequestMapping(method = [RequestMethod.GET],
+    @RequestMapping(
+            method = [RequestMethod.POST],
+            consumes = [MediaType.APPLICATION_JSON_VALUE],
             produces = [MediaType.APPLICATION_JSON_VALUE]
     )
-    fun findAll(): List<Person> {
-        return service.findAll()
+    fun create(@RequestBody person: Person): Person {
+        return service.create(person)
     }
 
+    @RequestMapping(
+            method = [RequestMethod.PUT],
+            consumes = [MediaType.APPLICATION_JSON_VALUE],
+            produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun update(@RequestBody person: Person): Person {
+        return service.update(person)
+    }
+
+    @RequestMapping(
+            value = ["/{id}"],
+            method = [RequestMethod.DELETE],
+            produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun delete(@PathVariable(value="id") id: Long) {
+        service.delete(id)
+    }
 }
--- Kotlin_REST_API/src/main/kotlin/br/com/erudio/Kotlin_REST_API/services/PersonService.kt
+++ Kotlin_REST_API/src/main/kotlin/br/com/erudio/Kotlin_REST_API/services/PersonService.kt
@@ -12,20 +12,6 @@ class PersonService {
 
     private val logger = Logger.getLogger(PersonService::class.java.name)
 
-    fun findById(id: Long): Person {
-
-        logger.info("Finding one person!")
-
-        val person = Person()
-        person.id = counter.incrementAndGet()
-        person.firstName = "Vinicius"
-        person.lastName = "Teixeira"
-        person.address = "Goiania - Goias - Brasil"
-        person.gender = "Male"
-
-        return person
-    }
-
     fun findAll(): List<Person> {
 
         logger.info("Finding all people!")
@@ -38,6 +24,26 @@ class PersonService {
         }
 
         return persons
+    }
+
+    fun create(person: Person) = person
+
+    fun update(person: Person) = person
+
+    fun delete(id: Long) {}
+
+    fun findById(id: Long): Person {
+
+        logger.info("Finding one person!")
+
+        val person = Person()
+        person.id = counter.incrementAndGet()
+        person.firstName = "Vinicius"
+        person.lastName = "Teixeira"
+        person.address = "Goiania - Goias - Brasil"
+        person.gender = "Male"
+
+        return person
     }
 
     private fun mockPerson(i: Int): Person {
