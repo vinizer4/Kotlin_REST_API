63eec461d257a9da895f2a0ea8dcc0ac23b745b1
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-06-25 00:12:16 -0300

    feat: Add SWAGGER

--- Kotlin_REST_API/src/main/kotlin/br/com/erudio/Kotlin_REST_API/data/dto/v2/PersonDTO.kt
+++ Kotlin_REST_API/src/main/kotlin/br/com/erudio/Kotlin_REST_API/data/dto/v2/PersonDTO.kt
@@ -2,10 +2,12 @@ package br.com.erudio.Kotlin_REST_API.data.dto.v2
 
 import br.com.erudio.Kotlin_REST_API.data.dto.v1.PersonDTO
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.github.dozermapper.core.Mapping
 import org.springframework.hateoas.RepresentationModel
 import java.util.*
 
+@JsonPropertyOrder("id", "firstName", "lastName", "address", "gender", "birthDay")
 class PersonDTO (
         @Mapping("id")
         @field:JsonProperty("id")
