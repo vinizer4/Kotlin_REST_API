ef6203a2bfc078a3bffb61c6041024954f02c670
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-06-25 16:54:11 -0300

    feat: created user service and repository

---
+++ Kotlin_REST_API/src/main/kotlin/br/com/erudio/Kotlin_REST_API/repositories/UserRepository.kt
@@ -0,0 +1,15 @@
+package br.com.erudio.Kotlin_REST_API.repositories
+
+
+import br.com.erudio.Kotlin_REST_API.models.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User?, Long?> {
+
+    @Query("SELECT u FROM User u WHERE u.userName =:userName")
+    fun findByUsername(@Param("userName") userName: String?): User?
+}
