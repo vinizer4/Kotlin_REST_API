227bedf2f167bb443669db7daf23dfc81478e966
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-06-25 17:45:50 -0300

    feat: created authService

---
+++ Kotlin_REST_API/src/main/kotlin/br/com/erudio/config/SecurityConfig.kt
@@ -0,0 +1,68 @@
+package br.com.erudio.config
+
+import br.com.erudio.security.jwt.JwtConfigurer
+import br.com.erudio.security.jwt.JwtTokenProvider
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig {
+
+    @Autowired
+    private lateinit var tokenProvider: JwtTokenProvider
+
+    @Bean
+    fun passwordEncoder() : PasswordEncoder {
+        val encoders: MutableMap<String, PasswordEncoder> = HashMap<String, PasswordEncoder>()
+        val pbkdf2Encoder = Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256)
+        encoders["pbkdf2"] = pbkdf2Encoder
+        val passwordEncoder = DelegatingPasswordEncoder("pbkdf2", encoders)
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder)
+        return passwordEncoder
+    }
+
+    @Bean
+    fun authenticationManagerBean(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
+        return authenticationConfiguration.authenticationManager
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity) : SecurityFilterChain {
+        return http
+            .httpBasic().disable()
+            .csrf {o: CsrfConfigurer<HttpSecurity> -> o.disable()}
+            .sessionManagement { session:
+                SessionManagementConfigurer<HttpSecurity?> ->
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+            .authorizeHttpRequests {
+                authorizeHttpRequests -> authorizeHttpRequests
+                    .requestMatchers(
+                        "/auth/signin",
+                        "/auth/refresh/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**")
+                    .permitAll()
+                    .requestMatchers("/api/**").authenticated()
+                    .requestMatchers("/users").denyAll()
+            }
+            .cors()
+            .and()
+            .apply(JwtConfigurer(tokenProvider))
+            .and()
+            .build()
+    }
+}
