fb5c46370541e0b6651d824bd7841cdf2aad0f79
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-06-25 15:50:46 -0300

    feat: add cors configs

--- Kotlin_REST_API/src/main/kotlin/br/com/erudio/Kotlin_REST_API/config/WebConfig.kt
+++ Kotlin_REST_API/src/main/kotlin/br/com/erudio/Kotlin_REST_API/config/WebConfig.kt
@@ -1,30 +1,27 @@
 package br.com.erudio.Kotlin_REST_API.config
 
 import br.com.erudio.Kotlin_REST_API.serialization.converter.YamlJackson2HttpMessageConverter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig: WebMvcConfigurer {
+class WebConfig : WebMvcConfigurer{
 
-    private val MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml")
+    private val MEDIA_TYPE_APPLICATION_YML =  MediaType.valueOf("application/x-yaml")
+
+    @Value("\${cors.originPatterns:default}")
+    private val corsOriginPatterns: String = ""
 
     override fun extendMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
         converters.add(YamlJackson2HttpMessageConverter())
     }
 
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
-//        configurer.favorParameter(true)
-//                .parameterName("mediaType")
-//                .ignoreAcceptHeader(true)
-//                .useRegisteredExtensionsOnly(false)
-//                .defaultContentType(MediaType.APPLICATION_JSON)
-//                    .mediaType("json", MediaType.APPLICATION_JSON)
-//                    .mediaType("json", MediaType.APPLICATION_XML)
-
 
         configurer.favorParameter(false)
                 .ignoreAcceptHeader(false)
@@ -33,5 +30,13 @@ class WebConfig: WebMvcConfigurer {
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
                 .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        val allowedOrigins = corsOriginPatterns.split(",").toTypedArray()
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(*allowedOrigins)
+                .allowCredentials(true)
     }
 }
